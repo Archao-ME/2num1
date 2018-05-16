486391b2c0b50d9b82659ebd0b553bbcd6ff5e00
jesusalexander <brpoper@gmail.com>
2018-05-16 16:46:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    margin: 0 auto;
+    width: 1440px;
+    height: 900px;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="trans-origin-wrapper">
+    <div class="roll-btn pre" @click="rollPre()">pre</div>
+    <div class="roll-btn next" @click="rollNext()">next</div>
+    <div class="item" v-for="(item, index) in itemList" :key="index" :class="'t-'+(index + currentIndex)" :attr-item="item"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      currentIndex: 0,
+      itemList: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+    }
+  },
+  methods: {
+    rollPre () {
+      this.currentIndex > 0 ? this.currentIndex = this.currentIndex - 1 : this.currentIndex = 0
+    },
+    rollNext () {
+      this.currentIndex < 19 ? this.currentIndex = this.currentIndex + 1 : this.currentIndex = 19
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .trans-origin-wrapper {
+    position: absolute;
+    bottom: 0;
+    height: 630px;
+    width : 1100px;
+    overflow: hidden;
+  }
+  .roll-btn {
+    display: inline-block;
+    width: 100px;
+    height: 30px;
+    background: #ccc;
+  }
+  .item {
+    position: absolute;
+    left: 50%;
+    height: 145px;
+    width: 136px;
+    background: #ccc;
+    transform-origin: 50% 552px;
+    opacity: 0.5;
+  }
+  @for $i from 0 through 30 {
+    .item.t-#{$i} {
+      $tNum: 1996 + $i;
+      $deg: $i*20 + deg;
+      @if $i < 19 {
+        $tNumBg: "../assets/big/t-" + $tNum + '.png';
+        transform:rotate($deg) scale(0.8, 0.8);
+        background: url($tNumBg);
+      }
+      background-size: cover;
+    }
+  }
+</style>
